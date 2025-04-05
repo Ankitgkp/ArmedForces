066922736eb40dd526c646e56f13c3dd9ac0f6f5
Personal <personal@Ankits-MacBook-Pro.local>
2025-04-05 19:30:06 +0530

    Added ArmyPage+

--- src/pages/Army/ArmyHome.jsx
+++ src/pages/Army/ArmyHome.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
-import armyHero from "../../assets/Pi7_pexels-chaikong2511-20258.jpg";
 /* eslint-disable no-unused-vars */
 import { motion, AnimatePresence } from "framer-motion";
 import StatisticsSection from "../../components/StatisticsSection";
 import WhyJoinArmy from "../../components/WhyJoinArmy";
 import ArmyRegimentsMarquee from "../../components/ArmyRegimentsMarquee";
 import Gallery from "../../components/Gallery";
+import JoinButton from "../../components/JoinButton";
+import Footer from "../../components/Footer";
 
 const ArmyHome = () => {
   const [autoChange, setAutoChange] = useState(false);
 
   useEffect(() => {
-    document.body.style.backgroundColor = "black";
+    document.body.style.backgroundColor = "#2B2E31";
     return () => {
       document.body.style.backgroundColor = "#1f1f1f";
     };
@@ -29,7 +30,7 @@ const ArmyHome = () => {
     <div>
     <div className="relative opacity-90">
       <img
-        src={armyHero}
+        src={'https://media-hosting.imagekit.io/c845a7b438684e81/Pi7_pexels-chaikong2511-20258.jpg?Expires=1838469542&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=gnlSeUd5kbSqwKlXQjUnya3yDPvSJFNivZBjN~A~Z2AT5EbeGMyYb5ZoMHmAxUnTRasB7dA4wJOVBC2crCdK9O8zGXfCC87abpvUZIb7J2UuNsGFUOssMgrUu0~57yah8aI5Y6bKTPCLbroX-pTP7UpFd8agrWZmtSbNhCvimn2FhmBUII-yrrlQPwh30bXp4WyPF95mOdHVU-3gJoh01hTBhZQ1rXzHWBMgOhx00orp4R2JOcOxtbyzt75OxlvTAFkzN7ijUP7EyOL0JIL4SKl2dmtGqfYlXaqRe8mzdqpVO3PBXdYGrTMRZcMpzBzU40XiUCC4rspY8PKwSrPb1Q__'}
         alt="Army Hero"
         className="w-full h-[100vh] object-cover"
       />
@@ -61,6 +62,9 @@ const ArmyHome = () => {
     <StatisticsSection />
     <WhyJoinArmy />
     <Gallery />
+    <JoinButton />
+    <ArmyRegimentsMarquee />
+    <Footer />
     </div>
   );
 };
